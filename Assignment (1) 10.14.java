bf7e2f8502186b6becd8d924c4d817a9c0a1a195
Alexa Scott <99158641+AlexaRosebud@users.noreply.github.com>
2023-11-05 12:33:14 -0500

    Add files via upload
    https://lucid.app/lucidchart/b05c7503-d34f-473a-b317-2fdbf0cbab17/view?page=0_0&invitationId=inv_8d8bed90-9582-4bba-a758-261072c1cf57#

---
+++ Assignment (1) 10.14.java
@@ -0,0 +1,18 @@
+package Chapter_10;
+
+import ToolKit.MyDate;
+
+public class Exercise_14 {
+
+    public static void main(String[] args) {
+
+        MyDate date1 = new MyDate();
+        MyDate date2 = new MyDate(34355555133101L);
+
+        System.out.println("Date1 - Month: " + date1.getMonth() +
+                " Day: " + date1.getDay() + " Year: " + date1.getYear());
+        System.out.println("Date2 - Month: " + date2.getMonth() +
+                " Day: " + date2.getDay() + " Year: " + date2.getYear());
+
+    }
+}
